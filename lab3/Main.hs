d20ee44ce08adededf69e151869f162a1a0a36fc
Atanasiu Alexandru <alexandru.atanasiu@outlook.com>
2015-03-20 12:56:31 +0200

    Lab3 PP Update [IMAGINI]

--- lab3/Main.hs
+++ lab3/Main.hs
@@ -19,3 +19,49 @@ matTranspose m = (map head m) : matTranspose (map tail m)
 
 -- 5. Implement the multiplication of two matrixes: m x m'.
 matMultiply m m' = map (\l -> map (\l' -> foldr (+) 0 (zipWith (*) l l')) (matTranspose m')) m
+
+
+--LUCRU CU IMAGINI
+
+l1="        ***** **            ***** **    "
+l2="     ******  ****        ******  ****   "
+l3="    **   *  *  ***      **   *  *  ***  "
+l4="   *    *  *    ***    *    *  *    *** "
+l5="       *  *      **        *  *      ** "
+l6="      ** **      **       ** **      ** "
+l7="      ** **      **       ** **      ** "
+l8="    **** **      *      **** **      *  "
+l9="   * *** **     *      * *** **     *   "
+l10="      ** *******          ** *******    "
+l11="      ** ******           ** ******     "
+l12="      ** **               ** **         "
+l13="      ** **               ** **         "
+l14="      ** **               ** **         "
+l15=" **   ** **          **   ** **         "
+l16="***   *  *          ***   *  *          "
+l17=" ***    *            ***    *           "
+l18="  ******              ******            "
+l19="    ***                 ***             "
+
+img = [l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18,l19]
+
+--flip vertical
+vertical_flip i = foldl (\x y -> y:x) [] i
+
+--flip orizontal
+horizontal_flip i = map (\x -> reverse x) i
+
+--afisare
+display m = putStr (foldr (++) "" (map (++ "\n") m))
+
+--rotatie -90*
+rotate_right i = vertical_flip (transpunere i)
+
+--rotatie +90*
+rotate_left i = transpunere (vertical_flip i)
+
+--negative
+negative i = map (\x -> map (\y -> if y == ' ' then '*' else ' ') x) i
+
+--scale
+scale i sz = map (\x -> map (\string -> concat $ replicate sz string) x) i 
